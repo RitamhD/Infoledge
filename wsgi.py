f1cf9466f31d305168ea2ff9b31613ab1a334464
RitamhD <hatiritam03@gmail.com>
2025-11-19 22:55:29 +0530

    demo

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,3 @@
 from app import create_app
 
-<<<<<<< HEAD
 app = create_app()
-
-if __name__ =='__main__':
-    app.run(debug=True, use_reloader=False)
-=======
-app = create_app()
->>>>>>> 5f0416db394212f08a47de8aa1e66ce78c310935
